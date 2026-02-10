28c517ae40514de31011b7bc4ad43a36946fc393
Leonardo Berbert <leo4berbert@gmail.com>
2026-02-09 22:52:53 -0300

    🐛 fix: Adjust Log Debug Actions

--- locale/src/zashterminal/settings/config.py
+++ locale/src/zashterminal/settings/config.py
@@ -32,7 +32,7 @@ class AppConstants:
 
     APP_ID = "org.leoberbert.zashterminal"
     APP_TITLE = "Zashterminal"
-    APP_VERSION = "0.1.17"
+    APP_VERSION = "0.1.18"
     DEVELOPER_NAME = "Leonardo Berbert"
     DEVELOPER_TEAM = ["Leonardo Berbert"]
     COPYRIGHT = "© 2025 Leonardo Berbert"
