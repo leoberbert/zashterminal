f52953e2ab22c7c05546ef21658aaa231d75722f
Leonardo Berbert <leo4berbert@gmail.com>
2026-02-09 22:51:24 -0300

    🐛 fix: More Debug

--- src/zashterminal/settings/config.py
+++ src/zashterminal/settings/config.py
@@ -35,7 +35,7 @@ class AppConstants:
 
     APP_ID = "org.leoberbert.zashterminal"
     APP_TITLE = "Terminal Zash"
-    APP_VERSION = "0.1.10"
+    APP_VERSION = "0.1.16"
     DEVELOPER_NAME = "Leonardo Berbert"
     DEVELOPER_TEAM = ["Leonardo Berbert"]
     COPYRIGHT = "© 2025 Leonardo Berbert"
